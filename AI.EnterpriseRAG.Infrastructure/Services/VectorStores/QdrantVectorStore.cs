02dbcef6ff92724b1ae5ac4667cf0c32bc9355c6
GLOBAL\H381850 <Yushan.Wu@Honeywell.com>
2026-03-09 17:02:14 +0800

    Qdrant 向量库实现

---
+++ AI.EnterpriseRAG.Infrastructure/Services/VectorStores/QdrantVectorStore.cs
@@ -0,0 +1,48 @@
+﻿using AI.EnterpriseRAG.Domain.Entities;
+using AI.EnterpriseRAG.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.EnterpriseRAG.Infrastructure.Services.VectorStores
+{
+    public class QdrantVectorStore : IVectorStore
+    {
+        public Task BatchDeleteByDocumentIdsAsync(List<Guid> documentIds, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ClearAllAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteByDocumentIdAsync(Guid documentId, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteExpiredAsync(DateTime expireTime, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> InitAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync(DocumentChunk chunk, float[] vector, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<DocumentChunk>> SearchAsync(float[] queryVector, int topK = 3, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
